92d0dc4f401dbe8b6f4b1315f8a0d21081f014f7
aduck <781957583@qq.com>
2015-09-09 19:39:02 +0800

    新增屏蔽按钮api

--- jquery.cars.js
+++ jquery.cars.js
@@ -1,15 +1,25 @@
 ;(function($){
 	$.fn.extend({
-		shopCars:function(){
-			var $cars_btn=$(".J_cars_btn"),
-				$cars_show=$(".J_cars_num"),
+		shopCars:function(opts){
+			var setting={
+				btn:$(this),
+				show:$(".J_cars_num"),  //购物车条数
+				thumb:null,  //商品缩略图
+				speed:1000,  //滑行速度
+				disabled:true  //是否禁用按钮
+			};
+			opts=$.extend(setting,opts);
+			var $cars_btn=opts.btn,
+				$cars_show=opts.show,
 				btn_off_left=$cars_btn.offset().left,
 				btn_off_top=$cars_btn.offset().top,
 				show_off_left=$cars_show.offset().left,
 				show_off_top=$cars_show.offset().top;
 			$cars_btn.click(function(){
 				//禁用按钮
-				$cars_btn.get(0).disabled=true;
+				if(opts.disabled){
+					this.disabled=true;
+				}
 				//发送ajax请求，添加到购物车
 				$.ajax({
 					url:"",
@@ -24,27 +34,31 @@
 					}
 				});
 				//新建图层
-				var $layer=$("<div></div>");
+				var $layer=$("<div></div>"),
+					$thumb=$("<img src="+opts.thumb+" />").css("max-width","100%");
+				$thumb.appendTo($layer);
 				$layer.css({
 					"position":"absolute",
-					"left":"0",
-					"top":"0",
-					"width":"20px",
-					"height":"20px",
-					"border-radius":"50%",
-					"background":"#ccc"
-				}).appendTo($cars_btn);	
-				$cars_btn.css("position","relative");
+					"z-index":"999",
+					"left":btn_off_left,
+					"top":btn_off_top,
+					"width":"80px",
+					"height":"80px",
+					"border":"2px solid #ccc",
+					"overflow":"hidden"
+				}).appendTo($("body"));	
 				//开启动画
 				$layer.animate({
-					"left":parseInt(show_off_left)-parseInt(btn_off_left)+"px",
-					"top":parseInt(show_off_top)-parseInt(btn_off_top)+"px"
-				},500,function(){
+					"left":show_off_left,
+					"top":show_off_top,
+					"opacity":"0"
+				},opts.speed,function(){
 					$layer.remove();
 					$cars_show.text(parseInt($cars_show.text())+1);
 				});
 				return false;
 			});
+			return this;
 		}
 	});
 })(jQuery);
